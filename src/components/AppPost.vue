09017c6cad7cbadde7a4200c36aadfffb13548ae
sirijiji <alexandre.boungnarith@gmail.com>
2017-01-11 00:46:00 +0100

    add new components

---
+++ src/components/AppPost.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="apppost">
+    <titlehero></titlehero>
+    <navmenu></navmenu>
+    <listblogpost></listblogpost>
+    <footerblog></footerblog>
+  
+  </div>
+</template>
+<script>
+
+module.exports = {
+  name: 'apppost',
+  components: {
+    
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+  color: #2c3e50;
+
+}
+</style>
---
+++ src/components/ListPostBlog.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="section">
+    <div class="container">
+      
+      <router-link to="/sectionblog/post" replace>
+        <h1 class="title linktitle">{{ msg }}</h1>
+        <h3 class="subtitle linktitle">10/01/2017</h3>
+      </router-link>
+      <div class="separator"> 
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+  <h2>Second level</h2>
+  <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
+  <ul>
+    <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+    <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+    <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
+    <li>Ut non enim metus.</li>
+  </ul>
+      </div>  
+       
+    </div>
+  </section>
+</template>
+
+<script>
+module.exports = {
+  name: 'listblogpost',
+  data: function () {
+    return {
+      msg: 'Example Blog post'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.linktitle {
+ color: #00d1b2;  
+}
+
+.separator {
+  border-top : 1px solid #00d1b2;
+  margin-top: 5px;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+</style>
